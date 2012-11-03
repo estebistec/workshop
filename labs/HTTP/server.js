6953ba40c9884e41530135eab31966d8f15e6bc4
Steven Cummings <estebistec@gmail.com>
2012-11-03 11:59:15 -0500

    HTTP lab completed

--- labs/HTTP/server.js
+++ labs/HTTP/server.js
@@ -1,7 +1,29 @@
 var fs = require('fs'),
     http = require('http');
 
-function logRequest(request, response) {
+var templateEngine = function(template, data) {
+  var vars = template.match(/\{\w+\}/g);
+
+  if(vars === null) {
+    return template;
+  }
+
+  var nonVars = template.split(/\{\w+\}/g);
+  var output = '';
+
+  for(var i = 0; i < nonVars.length; i++) {
+    output += nonVars[i];
+
+    if(i < vars.length) {
+      var key = vars[i].replace(/[\{\}]/g, '');
+      output += data[key];
+    }
+  }
+
+  return output;
+};
+
+var logRequest = function(request, response) {
   // TODO also log extra response info (e.g., 302's location)
   console.log(request.method + " " + request.url + " HTTP/" + request.httpVersion +
       " => " + response.statusCode);
@@ -11,14 +33,20 @@ function logRequest(request, response) {
       console.log("\t" + headerName + ": " + request.headers[headerName]);
     }
   }
-}
+};
 
 var server = http.createServer(function(request, response) {
     response.statusCode = 200;
 
-    fs.readFile('index.html', function(err, data) {
+    fs.readFile('index.html', function(err, fileData) {
       if (!err) {
-        response.write(data);
+        response.write(templateEngine(fileData.toString(), {
+          name: 'Ryan Dahl',
+          node: process.versions.node,
+          v8: process.versions.v8,
+          time: new Date(),
+          url: request.url
+        }));
         response.end();
       }
       else {
